683f81b65f7825e11c20111b7f622c37f828f6f8
Hazel Wu <tinazx056@gmail.com>
2023-04-19 00:57:57 +0800

    feat: install & setting chakra ui

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import "./styles/globals.css";
+import { ChakraProvider } from '@chakra-ui/react'
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
@@ -14,7 +15,9 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <ThirdwebProvider activeChain={activeChain}>
-      <App />
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
     </ThirdwebProvider>
   </React.StrictMode>
 );
